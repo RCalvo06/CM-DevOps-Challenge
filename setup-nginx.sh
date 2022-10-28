c630fb3ee9da05fa9f5ce6a530b982f10d5caee0
RCalvo06 <ricardocalvo0601@gmail.com>
2022-10-27 21:25:58 -0600

    webpage

--- setup-nginx.sh
+++ setup-nginx.sh
@@ -12,4 +12,4 @@ sudo apt-get install nginx -y
     #Keeps nginx enabled
 sudo systemctl enable nginx
 
-
+echo "<h1>Ricardo</h1><h2>Alonso</h2><h3>Calvo</h3><h1>Proyecto DevOps IaC - UCreativa</h1>" | sudo tee /var/www/html/index.nginx-debian.html
